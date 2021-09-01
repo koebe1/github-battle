72f654dad64c302d3c892b3c5d1c0c86c579b9ad
koebe1 <koe.be.pay@web.de>
2021-09-01 08:47:37 +0200

    added state changes to popular component

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,13 +1,36 @@
 import React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage = selectedLanguage => {
+    this.setState({
+      selectedLanguage
+    });
+  };
   render() {
     const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python "];
     return (
       <ul className="flex-center">
         {languages.map(l => (
           <li key={l}>
-            <button className="btn-clear nav-link">{l}</button>
+            <button
+              className="btn-clear nav-link"
+              style={
+                l === this.state.selectedLanguage ? { color: "#9E9E9E" } : null
+              }
+              onClick={() => this.updateLanguage(l)}
+            >
+              {l}
+            </button>
           </li>
         ))}
       </ul>
