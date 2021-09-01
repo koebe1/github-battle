ddc3dffa6639f59a292fcf273e145e0a554389a0
koebe1 <koe.be.pay@web.de>
2021-09-01 10:11:13 +0200

    added prop-types type checking to LanguageNav component

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,4 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python "];
+  return (
+    <ul className="flex-center">
+      {languages.map(l => (
+        <li key={l}>
+          <button
+            className="btn-clear nav-link"
+            style={l === selected ? { color: "#9E9E9E" } : null}
+            onClick={() => onUpdateLanguage(l)}
+          >
+            {l}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+};
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -16,24 +41,16 @@ export default class Popular extends React.Component {
       selectedLanguage
     });
   };
+
   render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python "];
+    const { selectedLanguage } = this.state;
     return (
-      <ul className="flex-center">
-        {languages.map(l => (
-          <li key={l}>
-            <button
-              className="btn-clear nav-link"
-              style={
-                l === this.state.selectedLanguage ? { color: "#9E9E9E" } : null
-              }
-              onClick={() => this.updateLanguage(l)}
-            >
-              {l}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selected={this.state.selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
     );
   }
 }
